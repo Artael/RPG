e2f34fdbb766565297bcb0f48bbdc4b3fb8e73ed
llait <xxxshadowxxx.95@gmail.com>
2013-09-25 00:38:46 +0300

    test commit

--- src/Game.java
+++ src/Game.java
@@ -11,7 +11,7 @@ public class Game
 {
     public static void main(String[] args)
     {
-        System.out.println("Начало");
+        System.out.println("Begin");
         ViewSpell View = new ViewSpell();
         View.ViewSpell();
 
